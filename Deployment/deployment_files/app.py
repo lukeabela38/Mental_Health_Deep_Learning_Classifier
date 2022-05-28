dd2ee5a9eede736508bdd76562a956ef7179b409
luke <lukeabela56@gmail.com>
2022-05-28 14:54:25 +0200

    Deployment now features docker-compose

--- Deployment/deployment_files/app.py
+++ Deployment/deployment_files/app.py
@@ -2,10 +2,12 @@ import os
 import json
 import pandas
 from mentalhealth import MentalHealth
+
 from flask import Flask, request, jsonify
 
 mn = MentalHealth()
 app = Flask(__name__)
+
 
 @app.route("/read", methods = ["POST"])
 def read():
@@ -14,12 +16,15 @@ def read():
     df = mn._runMentalHealthAlarmSystem(text)
     return dict(zip(df['Disorder'], df['Percentage Chance of Disorder']))
 
+"""
+curl http://localhost:8000/read -d "text=Hello, this is a test"
+"""
 
-"""
-curl http://localhost:8001/read -d "text=Hello, this is a test"
-"""
+@app.route('/')
+def hello():
+    return "Mental Health Classifier using Social Media Generated Data"
+
 
 if __name__ == "__main__":
     from waitress import serve ## use waitress to eliminate flask production error
-    serve(app, host="0.0.0.0", port = 8001)
-
+    serve(app, host="0.0.0.0", port = 8000)
--- Deployment/deployment_files/gradio_demo.py
+++ Deployment/deployment_files/gradio_demo.py
@@ -4,4 +4,4 @@ from mentalhealth import MentalHealth
 if __name__ == "__main__":
     MN = MentalHealth()
     iface = gr.Interface(fn=MN._runMentalHealthAlarmSystem, inputs="text", outputs="dataframe")
-    iface.launch(server_name="0.0.0.0", server_port=8000, share=True)
+    iface.launch(server_name="0.0.0.0", server_port=8080, share=True)
